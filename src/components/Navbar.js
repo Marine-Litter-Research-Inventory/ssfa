8963aabefc420f325f695cdf6cffcc65d57a2b65
Yuuki <huy2840@gmail.com>
2021-06-05 23:03:16 +0800

    Setting up repo

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>SSFA</h1>
+      <div className="links">
+        <a href="/">Map</a>
+        <a href="/about">About</a>
+        <a href="/guidelines">Guidelines</a>
+        <a href="/other">Other</a>
+        <a href="/regional">Regional</a>
+      </div>
+    </nav>
+  );
+}
