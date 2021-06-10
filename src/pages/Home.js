6e3fedfdc842f1aec43c3e29671340a3b3883d82
Yuuki <huy2840@gmail.com>
2021-06-10 16:16:57 +0800

    Nothing much

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Typography, Container, Divider, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import NavCard from 'components/NavCard';
@@ -5,7 +6,7 @@ import NavCard from 'components/NavCard';
 const useStyles = makeStyles((theme) => ({
   background: {
     maxHeight: 300,
-    backgroundImage: `url("https://lh4.googleusercontent.com/1q8tVMwteA_OFzZ850YWeRqJITxo3lVFk18Tf6_k9A_L0G7HI_dHQBjRTmr-Avhdd3q6vGCBmZXpCH-VS7HHnlw=w16383")`,
+    backgroundImage: "url('/images/background.png')",
     backgroundSize: '200%',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
@@ -27,20 +28,24 @@ const useStyles = makeStyles((theme) => ({
 
 const cards = [
   {
+    id: 1,
     title: 'Sampling & Processing Guidelines',
-    src: 'https://lh3.googleusercontent.com/ejNobsXcDEkws_Ijd2Jsft6yBdi072iQQbtRErBzZoENkkp_4lzg7lOTi8IkjvTrMtswE0nNgNO_l2Dhnd0fb0o=w1280'
+    src: '/images/1.jpg'
   },
   {
+    id: 2,
     title: 'Regional Report of Legal and Policy Efforts',
-    src: 'https://lh5.googleusercontent.com/ftStvM0jKKm8O5zJFPBW1CwXrYMlLPp6_ofziSlpNAv0GvKpJ1nycIlHti_nYQUnWBK7j-9Glc_QXwALajyIzZ4=w1280'
+    src: '/images/2.jpg'
   },
   {
+    id: 3,
     title: 'Understanding Marine Plastic Research in SE-EA',
-    src: 'https://lh5.googleusercontent.com/dqiET7eWSd6NqzMNMqyAfbO02cBMqRmOT9Uwkem6cRU_uWrtm1xyGK8ptK-bQwAWYvpjDCYf77CxPUyVEMtpvw=w1280'
+    src: '/images/3.jpg'
   },
   {
+    id: 4,
     title: 'Other Data Repositories & Infographics',
-    src: 'https://lh5.googleusercontent.com/URdvHCxnBAuQ9n_ND7iaJGxaX78Jj3orSL-3cmAVXOtb6IHRBP3WMXaeA23lJ0sb0G6OskcXLaTlS97jgdN3iw=w1280'
+    src: '/images/4.jpg'
   },
 ]
 
@@ -75,9 +80,11 @@ export default function Home() {
         <br /><br />
         <Grid container spacing={2}>
           {cards.map(card => (
-            <Grid item xs={12} sm={3} md={3}>
-              <NavCard title={card.title} src={card.src} />
-            </Grid>
+            <React.Fragment key={card.id}>
+              <Grid item xs={12} sm={6} md={3}>
+                <NavCard title={card.title} src={card.src} />
+              </Grid>
+            </React.Fragment>
           ))}
         </Grid>
       </Container>
